f4381dab5606d87bb10029278bc75e17cb0af8e0
ranaalmuqati <ranaalmuqati@hotmail.com>
2019-08-23 00:51:27 +0300

    Day Three

---
+++ Day3.py
@@ -0,0 +1,17 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+x = 5 # x is of type int
+x = "Sally"  # x is of type string
+print(x)
+x, y, z, = "Orange", "Banana", "Cherry"
+print(x, y, z)
+x = "awesome"
+print("Python is " + x)
+x = "Python is "
+y = "awesome"
+print(x+y)
+x = 10
+y = 5
+print(x+y)
