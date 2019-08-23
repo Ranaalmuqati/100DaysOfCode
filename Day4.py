8127bdba5034a80a51adc2100bf6704ef15ca1c1
ranaalmuqati <ranaalmuqati@hotmail.com>
2019-08-23 22:06:49 +0300

    Day 4

---
+++ Day4.py
@@ -0,0 +1,19 @@
+import random
+
+x = 1  # int
+y = 2.8  # float
+z = 1j  # complex
+a = float(x)
+b = int(y)
+c = complex(x)
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
+
+print(random.randrange(1, 100))
+
+
+
