f8eca64ec91a1fa2e0c9417ffa654c95e54a0575
ranaalmuqati <ranaalmuqati@hotmail.com>
2019-08-27 21:41:25 +0300

    Day 8

---
+++ Day8.py
@@ -0,0 +1,15 @@
+a = " Hello, World! "
+# The strip() method removes any whitespace from the beginning or the end
+print(a.strip())
+a = "Hello, World!"
+# The len() function returns the length of a string
+print(len(a))
+# The lower() method returns the string in lower case
+print(a.lower())
+# The upper() method returns the string in upper case
+print(a.upper())
+# The replace() method replaces a string with another string
+print(a.replace("H", "J"))
+# The split() method splits the string into substrings
+print(a.split(","))
+
