0e6a3716165dc3673c21b40ee54272ca6f112250
ranaalmuqati <ranaalmuqati@hotmail.com>
2019-08-29 21:25:42 +0300

    Day 10

---
+++ Day10.py
@@ -0,0 +1,17 @@
+x = 8
+y = 2
+print(x * y)
+print(x // y)  # the floor division // rounds the result down to the nearest whole number.
+x = 8
+x += 4  # 8+4
+print(x)
+x = 5
+x **= 3  # 5*5*5
+print(x)
+x = 7
+y = 2
+print(x == y)  # returns False because 7 is not equal to 2
+x = 7
+y = 2
+print(x >= y)  # returns True because 7 is greater or equal to 2
+
