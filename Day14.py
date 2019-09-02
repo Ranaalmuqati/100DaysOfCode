070f362226df1477cdc2870c22b32081f8d433de
ranaalmuqati <ranaalmuqati@hotmail.com>
2019-09-02 21:21:31 +0300

    Day 14

---
+++ Day14.py
@@ -0,0 +1,14 @@
+this_list = ["apple", "banana", "cherry", "orange", "mango"]
+print(this_list[2:4])
+if "banana" in this_list:
+    print("Yes, banana in this_list")
+this_list0 = ["Rana"] * 4
+print(this_list0)
+
+this_list1 = [1, 2, 3, 4]
+this_list2 = [5.0, 6.0, 7.0, 8.0]
+this_list3 = this_list1 + this_list2
+print(this_list3)
+
+
+
