ed7f7b330f7a7437cdf6dda9f22537e3889e651b
ranaalmuqati <ranaalmuqati@hotmail.com>
2019-09-04 22:11:30 +0300

    Day 16

---
+++ Day16.py
@@ -0,0 +1,16 @@
+this_tuple = ("apple", "banana", "cherry")
+print(this_tuple)
+this_tuple = ()
+print(this_tuple)
+this_tuple = (6,)
+print(this_tuple)
+this_tuple = (2, 1.5, 6, 9)
+print(this_tuple)
+this_tuple = ("Rana", 5.6, 6, "رنا")
+print(this_tuple)
+print(this_tuple[3])
+for x in this_tuple:
+    print(x)
+print(this_tuple[0:3])
+
+
