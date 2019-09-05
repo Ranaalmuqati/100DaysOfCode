b971284522bfc54b22de5b707ca71b88279a4516
ranaalmuqati <ranaalmuqati@hotmail.com>
2019-09-06 02:23:48 +0300

    Day 18 and 19

---
+++ Day18and19.py
@@ -0,0 +1,13 @@
+students = ["Rana", "Aljohara", "Nouf", "Atheer", "Hend", "Sara"]
+students.sort()  # Sort the list alphabetically
+print(students)
+students.reverse()  # Reverse the order of the students list
+print(students)
+print(len(students))  # Return the number of element
+students.clear()  # Remove all elements from the list
+print(students)
+programming_languages = ("java", "python", "swift")
+if "python" in programming_languages:
+    print("Yes, python in programming languages")
+else:
+    print("No, it is not in programming languages")
