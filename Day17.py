8e43e840c3e44eb730246fb7923097183600ea06
ranaalmuqati <ranaalmuqati@hotmail.com>
2019-09-05 22:51:57 +0300

    Day 17

---
+++ Day17.py
@@ -0,0 +1,22 @@
+fruits_tuple = ("apple", "banana", "cherry")
+print(len(fruits_tuple))
+if "apple" in fruits_tuple:
+    print("Yes, apple in fruits tuple")
+this_tuple = ("python",) * 3
+print(this_tuple)
+x = (1, 3, 5, 7)
+x = x + (4, 6, 9)
+print(x)
+numbers = (1, 3, 3, 9, 10)
+numbers_tuple = tuple(numbers)
+print(numbers_tuple)
+x = numbers.count(3)
+print(x)
+z = numbers.index(9)
+print(z)
+
+
+
+
+
+
