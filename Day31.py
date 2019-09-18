f6c3ecf1ea44cfc51a819604bf7739e2547b6eeb
ranaalmuqati <ranaalmuqati@hotmail.com>
2019-09-18 22:50:38 +0300

    Day 31

---
+++ Day31.py
@@ -0,0 +1,19 @@
+def my_function():
+    print("Hello from function")
+
+
+my_function()
+
+def my_function(fname):
+    print("Hi " + fname)
+
+my_function("Rana")
+
+def my_function(country = "Norway"):
+    print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
+
