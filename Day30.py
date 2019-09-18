170c04cea2465b717fa95301303fe9b0f4eaee90
ranaalmuqati <ranaalmuqati@hotmail.com>
2019-09-18 22:20:03 +0300

    Day 30

---
+++ Day30.py
@@ -0,0 +1,20 @@
+for x in range(6):
+    print(x)
+for x in range(2, 6):  # print the values from 2 to 6 (but not including 6)
+    print(x)
+for x in range(2, 30, 3):  # increment value by adding a third parameter
+    print(x)
+for x in range(6):
+    print(x)
+else:  # print the values from 2 to 5 and print a message when the loop has ended
+    print("Finally finished!")
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+    for y in fruits:
+        print(x, y)
+
+
+
