68f6d5c58c9fded16656389e60e030da539344ed
ranaalmuqati <ranaalmuqati@hotmail.com>
2019-09-26 05:57:55 +0300

    Day 37

---
+++ Day37.py
@@ -0,0 +1,13 @@
+cars = ["Ford", "Volvo", "BMW"]
+print(cars)
+
+x = cars[0]  # Access the Elements of an Array.
+print(x)
+
+cars[0] = "Toyota"  # Modify the value of the first array item:
+print(cars)
+
+x = len(cars)  # The Length of an Array
+print(x)
+
+
