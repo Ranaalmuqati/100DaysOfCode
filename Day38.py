25ad8983af475c09b40b02aacec3b57b9ee91c9c
ranaalmuqati <ranaalmuqati@hotmail.com>
2019-09-26 06:14:07 +0300

    Day 38

---
+++ Day38.py
@@ -0,0 +1,12 @@
+cars = ["Ford", "Volvo", "BMW"]
+for x in cars:  # Looping Array Elements
+    print(x)
+
+cars.append("Honda")   # Adding Array Elements
+print(cars)
+
+cars.pop(3)  # You can use the pop() method to remove an element from the array
+print(cars)
+
+cars.remove("Volvo")    # You can also use the remove() method to remove an element from the array.
+print(cars)
