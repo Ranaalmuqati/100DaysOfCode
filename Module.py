9c2ea65dd6302c43e807ff8122306b825992fddb
ranaalmuqati <ranaalmuqati@hotmail.com>
2019-10-08 19:06:04 +0300

    Day 50

---
+++ Module.py
@@ -0,0 +1,9 @@
+
+import mymodule
+
+mymodule.greeting("Rana")
+
+
+a = mymodule.person["age"]
+print(a)
+
---
+++ mymodule.py
@@ -0,0 +1,9 @@
+def greeting(name):
+    print("Hello," + name)
+
+
+person = {
+    "name":  "Rana",
+    "age": 23,
+    "country": "Saudi Arabia"
+}
