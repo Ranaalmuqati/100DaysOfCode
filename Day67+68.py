2cbfb851f93ff958da66fb566a328c87330398ba
ranaalmuqati <ranaalmuqati@hotmail.com>
2019-10-26 03:08:25 +0300

    Day 67 + 68

---
+++ Day67+68.py
@@ -0,0 +1,14 @@
+print("Task one:")
+
+print("Enter The First latter of your name:")
+x = input()
+print("Enter The Last latter of your name:")
+y = input()
+print("The First latter is:", x, "And, The Last latter is:", y)
+
+print("Task Two:")
+
+name = "Ahmed Ali"
+balance = 53.44
+txt = "Dear {0}, Your Current balance is {1}$"
+print(txt.format(name, balance))
